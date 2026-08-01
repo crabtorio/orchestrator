8db522851d842e0de84023cf9b712b0757d55895
Lorenzo <medurilorenzo@proton.me>
2026-08-01 10:47:32 +0200

    removed shell quitting from outside

--- src/orchestrator/shell.rs
+++ src/orchestrator/shell.rs
@@ -1,10 +1,7 @@
 use std::{
     collections::VecDeque,
     io::{self, Write},
-    sync::{
-        Arc, Mutex,
-        atomic::{AtomicBool, Ordering::Relaxed},
-    },
+    sync::{Arc, Mutex},
 };
 
 use common_game::utils::ID;
@@ -13,15 +10,11 @@ use crate::orchestrator::Command::{self, Exit, StartAllPlanets, StartPlanet};
 
 pub struct Shell {
     user_queue: Arc<Mutex<VecDeque<Command>>>,
-    run_flag: Arc<AtomicBool>,
 }
 
 impl Shell {
-    pub fn new(user_queue: Arc<Mutex<VecDeque<Command>>>, run_flag: Arc<AtomicBool>) -> Self {
-        Self {
-            user_queue,
-            run_flag,
-        }
+    pub fn new(user_queue: Arc<Mutex<VecDeque<Command>>>) -> Self {
+        Self { user_queue }
     }
     pub fn run(&self) {
         loop {
@@ -29,20 +22,17 @@ impl Shell {
             io::stdout().flush().unwrap();
             let mut input = String::new();
             if let Ok(_) = io::stdin().read_line(&mut input) {
-                // May need to make the read_line non-blocking, so that the flag is checked periodically using a timer.
-                // Though during testing the orchestrator is faster at modyfing the run_flag than the shell is at reading it
                 match parse_command(input.trim().to_string()) {
                     Ok(command) => {
+                        if let Exit = command {
+                            break;
+                        }
                         self.user_queue.lock().unwrap().push_back(command);
                     }
                     Err(error) => {
                         println!("Error: {}", error);
                     }
                 }
-            }
-
-            if !self.run_flag.load(Relaxed) {
-                break;
             }
         }
     }
@@ -63,6 +53,7 @@ fn parse_command(mut input: String) -> Result<Command, String> {
                 arguments[0].parse::<ID>().map_err(|err| err.to_string())?,
             ))
         }
+        "quit" => Ok(Exit),
         _ => Err(format!("Command '{}' is invalid", command)),
     }
 }
