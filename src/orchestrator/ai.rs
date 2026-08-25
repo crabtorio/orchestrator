0e762dedc67e031e582b4a59a9a7ded7a4810dc7
Alessio <alessio.zanon@unitn.it>
2026-08-25 22:56:36 +0200

    More ai changes

--- src/orchestrator/ai.rs
+++ src/orchestrator/ai.rs
@@ -43,9 +43,8 @@ impl Ai for RichardRandom {
                 const ASTEROID_WEIGHT: i32 = 1;
 
                 while runflag.load(Relaxed) {
-                    let target: ID = rand::random_range(start-1..=end);
-                    let action: i32 =
-                        rand::random_range(1..=(NOTHING_WEIGHT + SUN_WEIGHT + ASTEROID_WEIGHT));
+                    let target:ID = rand::random_range(start..=end) as ID;
+                    let action: i32 = rand::random_range(1..=(NOTHING_WEIGHT + SUN_WEIGHT + ASTEROID_WEIGHT));
                     log::trace!("AI: RichardRandom rolled {} on {}", action, target);
 
                     let mut lock = ai_queue.lock().unwrap();
